f029e4ad7143f9bcf4f185f9bd2d43f421a11c61
pacifiista <naurasfh2020@gmail.com>
2025-09-30 14:24:39 +0700

    halo

--- Test.java
+++ Test.java
@@ -3,5 +3,6 @@ public class Test{
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
         System.out.println("Ini baris kode program kedua hehehe");
+        System.out.println("aku mau pulang pls");
     }
 }
